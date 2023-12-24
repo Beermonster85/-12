df4fbbe7a638025accbf51fd98765fc42f38bad8
Kirill <kiril-shitikov@yandex.ru>
2023-12-24 20:46:33 +0300

    ДЗ 12

---
+++ script.js
@@ -0,0 +1,74 @@
+
+const goods_cards = document.querySelector('.goods_cards');
+console.log (goods_cards);
+
+const card1 = document.querySelector('#card1');
+card1.addEventListener('click', () => {
+    const cartItems = document.querySelector('.cartItems');
+    cartItems.style.display="block";
+});
+
+const close1 = document.querySelector('#close1');
+close1.addEventListener ('click', () => {
+    const cartItems = document.querySelector('.cartItems');
+    cartItems.style.display = 'none';
+})
+
+const card2 = document.querySelector('#card2');
+card2.addEventListener('click', () => {
+    const cartItems2 = document.querySelector('.cartItems2');cartItems2.style.display="block";
+});
+
+const close2 = document.querySelector('#close2');
+close2.addEventListener ('click', () => {
+    const cartItems2 = document.querySelector('.cartItems2');
+    cartItems2.style.display = 'none';
+});
+
+
+const card3 = document.querySelector('#card3');
+card3.addEventListener('click', () => {
+    const cartItems3 = document.querySelector('.cartItems3');cartItems3.style.display="block";
+});
+
+const close3 = document.querySelector('#close3');
+close3.addEventListener ('click', () => {
+    const cartItems3 = document.querySelector('.cartItems3');
+    cartItems3.style.display = 'none';
+});
+
+const card4 = document.querySelector('#card4');
+card4.addEventListener('click', () => {
+    const cartItems4 = document.querySelector('.cartItems4');cartItems4.style.display="block";
+});
+
+const close4 = document.querySelector('#close4');
+close4.addEventListener ('click', () => {
+    const cartItems4 = document.querySelector('.cartItems4');
+    cartItems4.style.display = 'none';
+});
+
+
+const card5 = document.querySelector('#card5');
+card5.addEventListener('click', () => {
+    const cartItems5 = document.querySelector('.cartItems5');cartItems5.style.display="block";
+});
+
+const close5 = document.querySelector('#close5');
+close5.addEventListener ('click', () => {
+    const cartItems5 = document.querySelector('.cartItems5');
+    cartItems5.style.display = 'none';
+});
+
+
+const card6 = document.querySelector('#card6');
+card6.addEventListener('click', () => {
+    const cartItems6 = document.querySelector('.cartItems6');cartItems6.style.display="block";
+});
+
+const close6 = document.querySelector('#close6');
+close6.addEventListener ('click', () => {
+    const cartItems6 = document.querySelector('.cartItems6');
+    cartItems6.style.display = 'none';
+});
+
